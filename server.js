c9f4f4886033f6820216a3d101844bf5110ee4ae
alfredlam42 <alfred.lam.42@gmail.com>
2018-02-14 18:52:11 -0800

    refactor to have database working in route - still need to refactor

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const db = require('./db');
-const routes = require('./routes');
-
-// Server Setup
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-const port = process.env.PORT || 4200;
 
 // Initialize Database
 db.connect(process.env.DB_ENDPOINT, function(err){
@@ -19,12 +9,24 @@ db.connect(process.env.DB_ENDPOINT, function(err){
 		process.exit(1);
 	} else {
 		console.log('Connected to database.');
+		
+		const express = require('express');
+		const bodyParser = require('body-parser');
+
+		const routes = require('./routes');
+
+		// Server Setup
+		const app = express();
+		app.use(bodyParser.urlencoded({ extended: true }));
+		app.use(bodyParser.json());
+
+		const port = process.env.PORT || 4200;
+
+		// Initializing Routes
+		routes.init(app);
+
+		// Start Server
+		app.listen(port);
+		console.log('Magic happens on port', port);
 	}
 })
-
-// Initializing Routes
-routes.init(app);
-
-// Start Server
-app.listen(port);
-console.log('Magic happens on port', port);
